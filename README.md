0358c32f5711c03099a96b8f60b6e01c9fe53896
Zytronium <dastelljes@outlook.com>
2025-07-06 18:14:17 -0500

    Implemented type relations to projects and tasks and completed task 4

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ WIP readme
 - [X] ​1. Root Query (1/? pts)
 - [X] ​2. Resolve function and test query in graphiql (1/? pts)
 - [X] ​3. GraphQL ID type and Project Type (1/? pts)
-- [ ] ​4. Type relations (0/? pts)
+- [X] ​4. Type relations (1/? pts)
 - [ ] ​5. More on root queries: projects and tasks (0/? pts)
 - [ ] ​6. Connecting to mongoDB Atlas and create mongoose models (0/? pts)
 - [ ] ​7. Mutation (0/? pts)
@@ -18,10 +18,10 @@ WIP readme
 ###### Since everything except task 0 shows "0/ pts," I'll calculate progress as if everything is 1 point, for a total of 9 pts.
 
 - [ ] Readme
-- [ ] **Everything Done ✓** (4/9 pts) - 44.44%
+- [ ] **Everything Done ✓** (5/9 pts) - 55.56%
 
 >### Progress Goals:  
-> <strong>Saturday/Sunday: 55.56% (task 4)</strong>  
+> <strong>Saturday/Sunday: 55.56% (task 4) ✓</strong>  
 <em style="color: gray">Monday: 77.78% (task 6)</em>  
 <em style="color: gray">Tuesday: 100% (task 8)</em>
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,27 +1,39 @@
 #!/usr/bin/node
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLID } = require('graphql');
 const {GraphQLSchema} = require("graphql/type");
 const lodash = require('lodash');
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    project: {
+      type: ProjectType,
+      resolve(parent, args) {
+        return lodash.find(projects, { id: parent.projectId });
+      }
+    }
+  })
 });
 
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
-  }
-})
+    description: { type: GraphQLString },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return lodash.filter(tasks, { projectId: parent.id });
+      }
+    }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -49,12 +61,14 @@ const RootQuery = new GraphQLObjectType({
 const tasks = [
   {
     id: "1",
+    projectId: '1',
     title: 'Create your first webpage',
     weight: 1,
     description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
   },
   {
     id: "2",
+    projectId: '1',
     title: 'Structure your webpage',
     weight: 1,
     description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
