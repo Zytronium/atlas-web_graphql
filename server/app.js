b0bc419b276ab37562b41c8f1612267f963711ec
Zytronium <dastelljes@outlook.com>
2025-07-08 18:58:43 -0500

    added code to connect to the mongoDB database did first half of task 6; committing now because I need to reboot my computer due to sudden technical difficulties

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
+
+const uri = "mongodb+srv://user-42a:sc5xXMRjCb6k3zJCx3Rd@cluster0.wajln82.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri);
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+  });
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
